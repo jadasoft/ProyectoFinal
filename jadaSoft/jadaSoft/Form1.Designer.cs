424199c09fe8c7d97affd10c8b760c7f9a32d6f7
Abel Chillón Morán <doblecuenta1@gmail.com>
2017-01-23 15:30:13 +0100

    Mover botton

--- jadaSoft/jadaSoft/Form1.Designer.cs
+++ jadaSoft/jadaSoft/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(52, 86);
+            this.button1.Location = new System.Drawing.Point(103, 104);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
