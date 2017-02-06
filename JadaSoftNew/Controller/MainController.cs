000bef3b597894f22aca1897d23904a3cadf1670
David <davidmorenocool@gmail.com>
2017-02-06 16:19:29 +0100

    vdlñl

--- JadaSoftNew/Controller/MainController.cs
+++ JadaSoftNew/Controller/MainController.cs
@@ -26,7 +26,7 @@ namespace Controller
             mainView.Show();
            // db = new JADAEntities();
 
-            //initListeners();
+            //initListeners();//
         }
 
         /**
